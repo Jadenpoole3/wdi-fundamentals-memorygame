10ab7048f8ea9be6aee5c81f93e07ed558913059
Jadenpoole3 <jadenkinpool@yahoo.com>
2017-08-01 00:09:17 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,67 @@
+main.js
+console.log("Up and running!");
+ <script src="js/main.js"></script>
+alert('Hello, friends.');
+var cards = {"queen", "queen", "king", "king"};
+
+var checkForMatch = function {
+	this.(cardId[cardId].cardImage)
+if (cardsInPlay[0] === cardsInPlay[1]) {
+      console.log("You found a match!");
+  } else {
+      console.log("Sorry, try again.");
+}
+var flipCard = function (cardId) {
+	this.getAttribute(data-id);
+console.log("User flipped" + cards{cardId});
+      cardsInPlay.push(cards{cardId});
+      cards[cardId].rank
+      queen.push(cards[cardId].rank);
+      king.push(cards[cardId].rank);
+      console.log(cardImage);
+      console.log(suit);
+
+  }
+  var createBoard = function {
+   for (var i = 0; i < createBoard.length; i++) {
+   var newListItem = document.createElement('img');
+   document.cardElement.setAttribute('src', 'images/back.png');
+   document.cardElement.setAttribute('data-id', i);
+   document.cardElement.addEventListener('click', flipCard);
+   document.cardElement(game-board),appendChild(newItem);
+ }
+  }
+createBoard.function.call(createBoard);
+delete.function.cardId;
+  
+  var myArray [
+  {
+  	rank: "queen",
+  	suit: "hearts",
+  	cardImage: "images/queen-of-hearts.png",
+  },
+  {
+  	rank: "queen",
+  	suit: "diamonds",
+  	cardImage: "images/queen-of-diamonds.png",
+  },
+  {
+  	rank: "king",
+  	suit: "hearts",
+  	cardImage: "images/king-of-hearts.png",
+  },
+  {
+  	rank: "king",
+  	suit: "diamonds",
+  	cardImage: "images/king-of-diamonds.png",
+  }
+
+  ]
+
+};
+
+
+
+
+</body>
+</html>
